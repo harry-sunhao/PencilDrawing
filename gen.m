85748b5071ae1f7ffc3f0bb9968cbc4ba75f64e8
Harry <36096251+harry-sunhao@users.noreply.github.com>
2022-04-18 16:20:51 +0800

    Update gen.m

--- gen.m
+++ gen.m
@@ -204,30 +204,16 @@ function J = GenToneMap(im,Omega)
 %  
 %   Paras:
 %   @im        : input image ranging value from 0 to 1.
+%   @Omega     : Weights for tone map generation.
 %
     
     %% Parameters
     Ub = 225;
     Ua = 105;
-    
     Mud = 90;
-    
     DeltaB = 9;
     DeltaD = 11;
-    
-    % groups from dark to light
-%     1st group
-%     Omega1 = 42;
-%     Omega2 = 29;
-%     Omega3 = 29;
-%     2nd group
-%     Omega1 = 10;
-%     Omega2 = 40;
-%     Omega3 = 10;
-%     3rd group
-%     Omega1 = 76;
-%     Omega2 = 22;
-%     Omega3 = 2;
+
     %% Compute the target histgram
     histgramTarget = zeros(256, 1);
     total = 0;
@@ -262,10 +248,10 @@ function T = GenPencil(im, P, J,renderDepth)
 %   Compute the pencil map 'T'
 %  
 %   Paras:
-%   @im        : input image ranging value from 0 to 1.
-%   @P         : the pencil texture.
-%   @J         : the tone map.
-%
+%   @im          : input image ranging value from 0 to 1.
+%   @P           : the pencil texture.
+%   @J           : the tone map.
+%   @renderDepth :Trace the generated texture rendering several times
 
     %% Parameters
     theta = 0.2;
